a8fce82fad8def1d0a57f1d63cecfe3798146663
a_yoshino <a-yoshino@iret.co.jp>
2023-08-31 15:25:23 +0900

    ダークモード対応

--- app/src/main/java/com/example/study_jetpack/MainActivity.kt
+++ app/src/main/java/com/example/study_jetpack/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.study_jetpack
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,9 +22,13 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
 import com.example.study_jetpack.data.MessageCard
+import com.example.study_jetpack.ui.theme.TutorialTheme
 
 class MainActivity : ComponentActivity() {
     private val messages:Array<MessageCard> = arrayOf(
@@ -29,7 +39,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageList(messages)
+            TutorialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageList(messages)
+                }
+            }
         }
     }
 }
@@ -46,27 +60,60 @@ fun MessageList(messages: Array<MessageCard> = arrayOf()) {
 @Composable
 fun MessageColumn(message: MessageCard) {
     Column(
-        modifier = Modifier.padding(top=2.5.dp, bottom=2.5.dp)
+        modifier = Modifier.padding(top = 2.5.dp, bottom = 2.5.dp)
     ) {
-        Row (verticalAlignment = Alignment.CenterVertically) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
             Image(
                 painter = painterResource(message.img),
                 contentDescription = message.imgDescribe,
-                modifier = Modifier.size(200.dp).padding(start=3.0.dp, end=3.0.dp),
+                modifier = Modifier
+                    .size(200.dp)
+                    .padding(start = 3.0.dp, end = 3.0.dp),
                 contentScale = ContentScale.Crop,
             )
-            Column (modifier = Modifier.padding(start=3.0.dp, end=3.0.dp)) {
-                Text(text = message.title)
-                Text(text = message.body)
+            Spacer(modifier=Modifier.width(8.dp))
+            Column(modifier = Modifier.padding(start = 3.0.dp, end = 3.0.dp)) {
+                Text(
+                    text = message.title,
+                    color = MaterialTheme.colorScheme.secondary,
+                    fontSize = 30.sp,
+                    style = MaterialTheme.typography.titleMedium
+                )
+                Spacer(modifier=Modifier.height(10.dp))
+                Surface(
+                    shape = MaterialTheme.shapes.large,
+                    tonalElevation = 10.dp,
+                    color = Color(red = 234, blue = 252, green = 244)
+                ) {
+                    Text(
+                        text = message.body,
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.padding(5.dp)
+                    )
+                }
             }
         }
     }
 }
 
+@Preview(name = "Light Mode")
+@Preview(
+    showBackground = true,
+    uiMode=Configuration.UI_MODE_NIGHT_YES,
+    name="Dark Mode")
+@Composable
+fun MessageListPreviewDark() {
+     val testMessages = arrayOf(
+        MessageCard("テスト１", "テストテスト", R.drawable.ic_launcher_foreground, "テスト画像1"),
+        MessageCard("テスト2", "テストテストてすとてすと", R.drawable.ic_launcher_foreground,"テスト画像2")
+    )
+    MessageList(testMessages)
+}
+
 @Preview(showBackground = true)
 @Composable
 fun MessageListPreview() {
-     val testMessages = arrayOf(
+    val testMessages = arrayOf(
         MessageCard("テスト１", "テストテスト", R.drawable.ic_launcher_foreground, "テスト画像1"),
         MessageCard("テスト2", "テストテストてすとてすと", R.drawable.ic_launcher_foreground,"テスト画像2")
     )
--- app/src/main/java/com/example/study_jetpack/ui/theme/Theme.kt
+++ app/src/main/java/com/example/study_jetpack/ui/theme/Theme.kt
@@ -38,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun Study_jetpackTheme(
+fun TutorialTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
